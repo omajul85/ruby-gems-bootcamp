a554f83b2d322a3e2d425e3dd9588cb3b7d691d4
Omar Alvarez <omajul2@hotmail.com>
2021-02-26 15:43:42 +0000

    Add rolify gem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  rolify
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
 
   has_many :courses
+
+  after_create :assign_default_role
 
   def to_s
     email
@@ -13,5 +15,12 @@ class User < ApplicationRecord
 
   def username
     email.split(/@/).first
+  end
+
+  private
+
+  def assign_default_role
+    self.add_role(:student) if self.roles.blank?
+    self.add_role(:teacher)
   end
 end
